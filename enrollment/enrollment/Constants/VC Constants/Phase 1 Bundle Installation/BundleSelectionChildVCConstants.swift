d6778991c5fc0a2abb5f6cb6fcf5539a2b5f596d
Jay Latman <jalatman@icloud.com>
2018-11-05 09:31:07 -0600

    update code documentation for bundle selection child view controller constants

--- enrollment/enrollment/Constants/VC Constants/Phase 1 Bundle Installation/BundleSelectionChildVCConstants.swift
+++ enrollment/enrollment/Constants/VC Constants/Phase 1 Bundle Installation/BundleSelectionChildVCConstants.swift
@@ -9,6 +9,19 @@
 
 import Foundation
 
+/**
+ Structure definition for the bundle selection view.
+ 
+ • header : string value for the first line of text from the top of the view
+ 
+ • subheader : string value for the second line of text from the top of the view
+ 
+ • recommended_label : string value use for ib label objects
+ 
+ • `environment` : [Keys] key name referenced for determining the environment being used to test for network reachability
+ 
+ child structs : Toggle
+ */
 struct BundleSelectionChildVC_Constants {
     static let header = "App bundles to get you started"
     static let subheader = "Additional apps can be installed from Self Service at any time."
@@ -29,11 +42,11 @@ struct BundleSelectionChildVC_Constants {
          Structure definition containing reference assignments of stored values or property list key names for retrieving values.
          - Note: The following references map to string values to be applied to each checkbox toggle button title
          
-         • `a`: first checkbox button title
+         • a : first checkbox button title
          
-         • `b`: second checkbox button title
+         • b : second checkbox button title
          
-         • `c`: third checkbox button title
+         • c : third checkbox button title
          */
         struct Title {
             static let a = "Connectivity"
@@ -44,11 +57,11 @@ struct BundleSelectionChildVC_Constants {
          Structure definition containing reference assignments of stored values or property list key names for retrieving values.
          - Note: The following references map to string values to be applied below each checkbox toggle button title
          
-         • `a`: first checkbox description
+         •  a : first checkbox description
          
-         • `b`: second checkbox description
+         •  b : second checkbox description
          
-         • `c`: third checkbox description
+         •  c : third checkbox description
          */
         struct Description {
             static let a = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
