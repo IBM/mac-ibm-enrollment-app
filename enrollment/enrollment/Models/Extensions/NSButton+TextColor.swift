26e93bd9239b7c2ccf514b0fa3ef7905b76e19c6
Jay Latman <jalatman@icloud.com>
2018-11-09 15:23:53 -0600

    add code documentation for NSButton extension

--- enrollment/enrollment/Models/Extensions/NSButton+TextColor.swift
+++ enrollment/enrollment/Models/Extensions/NSButton+TextColor.swift
@@ -11,6 +11,13 @@
 import AppKit
 
 extension NSButton {
+    
+    /**
+     Extension method for NSButton to change the text color and underline style. An example would be a button that mirrors an underlined URL link with no border.
+     
+     - Parameter color : NSColor for the string attributed to the button title
+     - Parameter underlineStyle : NSUnderlineStyle
+    */
     func set(textColor color: NSColor, underline underlineStyle: NSUnderlineStyle.RawValue) {
         let newAttributedTitle = NSMutableAttributedString(attributedString: attributedTitle)
         let range = NSRange(location: 0, length: attributedTitle.length)
@@ -23,6 +30,12 @@ extension NSButton {
         attributedTitle = newAttributedTitle
     }
     
+    /**
+     Extension method for applying an underline style to a NSButton. The color is hardcoded in the method.
+     
+     - Parameter toggleLabel : string value to be applied to the title attribute
+     - Parameter underline : integer value for NSUnderlineStyle
+    */
     func set(toggleLabel: String, underline: Int) {
         self.title = toggleLabel
         self.set(textColor: NSColor.headerTextColor, underline: NSUnderlineStyle.init(rawValue: underline).rawValue)
