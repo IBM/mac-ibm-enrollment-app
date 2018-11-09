452807dcb6b3e7990f1ed2f494c7114be4fa295f
Jay Latman <jalatman@icloud.com>
2018-11-09 15:22:41 -0600

    add code documentation for defining an app bundle info popover term

--- enrollment/enrollment/Models/DefineBundleInfoPopover.swift
+++ enrollment/enrollment/Models/DefineBundleInfoPopover.swift
@@ -9,6 +9,9 @@
 
 import AppKit
 
+/**
+ Class definition for defining a software bundle info popover
+ */
 class DefineBundleInfoPopover {
     
     var bundleTitle = String()
@@ -17,6 +20,17 @@ class DefineBundleInfoPopover {
     var appDescriptions = [String]()
     var appIcons = [NSImage]()
     
+    /**
+     Class initializer for defining a bundle info popover.
+     
+     The appTitles, appDescriptions, and appIcons construct individual application cards based on array index position.
+     
+     - Parameter bundleTitle : string value for the bundle's name
+     - Parameter bundleDescription : string value for a short description about the bundle (size is limited)
+     - Parameter appTitles : [Array] of strings containing all the apps in a bundle
+     - Parameter appDescriptions : [Array] of strings containing short descriptions for each app in the bundle (size is limited)
+     - Parameter appIcons : [Array] of images from the asset catalog
+    */
     init(bundleTitle: String, bundleDescription: String, appTitles: [String], appDescriptions: [String], appIcons: [NSImage]) {
         self.bundleTitle = String(bundleTitle)
         self.descriptionText = String(bundleDescription)
