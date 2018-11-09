9d404846fcdccc7649266fbebda409785e90ad86
Jay Latman <jalatman@icloud.com>
2018-11-09 15:20:26 -0600

    add code documentation for the bundle install button visbility

--- enrollment/enrollment/Models/BundleInstallButtonVisibility.swift
+++ enrollment/enrollment/Models/BundleInstallButtonVisibility.swift
@@ -11,6 +11,16 @@ import Foundation
 import Cocoa
 
 struct BundleInstallButtonVisibility {
+    /**
+     Method for handling the UI controls for the Bundle Selection view
+     
+     - Parameter label : NSTextField for the `totalDownloadTime`
+     - Parameter labelState : Boolean value for whether the label should be displayed
+     - Parameter installButton : NSButton for advancing to the installation screen
+     - Parameter installButtonState : Boolean value for whether the `installButton` should be displayed
+     - Parameter skipButton : NSButton for advancing to the SetupComplete / education view
+     - Parameter skipButtonState : Boolean value for whether the `skipButton` should be displayed
+     */
     static func displayButtons(label: NSTextField, labelState: Bool, installButton: NSButton, installButtonState: Bool, skipButton: NSButton, skipButtonState: Bool) {
         
         label.isHidden = labelState
